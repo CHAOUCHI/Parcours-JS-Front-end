8d61fc1bee64107587361b33c5a1cdb5e63cd2a0
CHAOUCHI <massi.chaouchi@gmail.com>
2023-02-03 09:24:05 +0100

    v1

--- monUltimeTodoList/v1/script.js
+++ monUltimeTodoList/v1/script.js
@@ -1,31 +1,100 @@
-/**
- * element.parentElement
- * element.remove()
- */
 
-// 1. Je recupere le forumlaire de saisi d'un nouvelle tache
-const form = document.querySelector("#form");
-// 2. Je recupere le conteneur des taches
+const nameTodoList = document.querySelector("#name-todo-list");
+const btnSaveList = document.querySelector("#save-todo");
+const btnClearList = document.querySelector("#clear-todo");
+
+const formtask = document.querySelector("#form");
+
 const listTasks = document.querySelector(".list-tasks");
 
-// 3. Lorsque le formulaire est soumis
-form.addEventListener("submit",function(e){
-    // 4. J'annule le comportement par défaut du formulaire
-    e.preventDefault();
-    // 5. Je recupere la valeur du champ input
+const loadListMenu = document.querySelector(".modal");
+
+
+loadAllTodoLists();
+
+formtask.addEventListener("submit",onAddTask);
+loadListMenu.addEventListener("click",onSelectTodoList);
+
+btnSaveList.addEventListener("click",onSaveList);
+btnClearList.addEventListener("click",onClearList);
+
+function onAddTask(event){
+    event.preventDefault();
     const  todoValue = document.getElementById("todo").value;
+    addTask(todoValue);
+    form.reset();
+}
 
+function onSelectTodoList(event){
+    const ls_listes = getLSListes();
+    const listId = event.target.getAttribute("data-listid");
 
-    let task = {
-        texte : todoValue,
-        cross : false
-    };
+    nameTodoList.value =  ls_listes[listId].name;
+    nameTodoList.setAttribute("data-listid",listId);
 
-    // 6. Je rajoute un nouvelle element à la suite des taches du conteneur de taches
+    const tasks = ls_listes[listId].tasks;
+
+    listTasks.innerHTML = "";
+    tasks.forEach(function(task){
+       addTask(task.text,task.cross);
+    });
+
+    burger.click();
+}
+
+function onSaveList(event){
+    const currentTodoList = getCurrentTodoList();
+    const currentListId = getCurrentListId();
+
+    const errorMsg = nameTodoList.nextElementSibling;
+    const successMsg = document.querySelector(".list-save");
+
+    const ls_todoListes = getLSListes();
+
+    if(currentTodoList.name.length < 2){
+        errorMsg.classList.remove("invisible");
+        return;
+    }
+    else
+        errorMsg.classList.add("invisible");
+
+    if(currentListId !== ""){
+        ls_todoListes[currentListId] = currentTodoList;
+    }else{
+        ls_todoListes.push(currentTodoList);
+    }
+
+    localStorage.setItem("todo-listes",JSON.stringify(ls_todoListes));
+
+    successMsg.classList.remove("invisible");
+    setTimeout(() => {
+        successMsg.classList.add("invisible");    
+    },3000);
+
+    loadAllTodoLists();
+}
+
+function onClearList(){
+    nameTodoList.value = "";
+    form.reset()
+    listTasks.innerHTML = "";
+}
+function loadAllTodoLists(){
+    console.log("Loading todo list...");
+    const ls_todoListes = getLSListes();
+    loadListMenu.innerHTML = ``;
+
+    ls_todoListes.forEach(function(todoList,index){
+        loadListMenu.innerHTML += `<a class="listid" data-listid="${index}">${todoList.name}</a>`
+    })
+    console.log("Listes loaded");
+}
+
+function addTask(text,cross){
     listTasks.innerHTML+=
     `
-    <div class="task">
-        <p>${task.texte}</p>
+    <div class="task ${cross?'done':''}">
+        <p>${text}</p>
         <button class="btn-delete">
             <i class="fas fa-trash-alt"></i>
         </button>
@@ -35,56 +104,34 @@ form.addEventListener("submit",function(e){
     </div>
     `;
 
-    // 7. Je recupere tout les boutons de suppressions de tache
     const btnsDelete = document.querySelectorAll(".btn-delete");
-    // 8. Pour chaque bouton, je supprime la tache correspondante, au clique
     btnsDelete.forEach((btn)=>{
-        // J'ecoute le clique
         btn.addEventListener("click",function(){
-            const task = btn.parentElement; // Je recupere l'element parent au bouton soit la tache.
+            const task = btn.parentElement; // Je recupere l'element parent au bouton soit la Task.
             task.remove(); // Je supprimme la tache
         })
     });
 
-    // 9. Je recupere tout les boutons archiver
     const btnsArchive = document.querySelectorAll(".btn-archive");
-    // 10. Pour chaque boutons, je barre la tache, au clique
     btnsArchive.forEach((btn)=>{
-        // J'ecoute le clique
         btn.addEventListener("click",function(){
-            //btn.parentElement.classList.toggle("done")
-            /**
-             * get corresponding task
-             * toggle task.cross
-             * if(task.cross)
-             *  btn.parentElement.classList.add("done")
-             * else
-             *  btn.parentElement.classList.remove("done")
-             */
+            btn.parentElement.classList.toggle("done");
         })
     });
-    // 11. Je reset le contenu du formulaire pour eviter que le texte reste une fois la tache crée (ergonomique)
-    form.reset();
-});
+}
 
+function getLSListes(){
+    const ls_todoListes_JSON = localStorage.getItem("todo-listes") || "[]";
+    return JSON.parse(ls_todoListes_JSON);
+}
 
 function getCurrentTodoList(){
     const currentTasks_HTML = document.querySelectorAll(".task");
     const tasks = Array.prototype.map.call(currentTasks_HTML,(task)=>{
         return {text:task.innerText,cross: task.classList.contains("done") }
-      })
-    const todoList = {name:getTodoListName(),tasks:tasks};
-    return todoList;
+    });
+    return {name:nameTodoList.value.trim(),tasks:tasks};
 }
-
-function getTodoListName(){
-    return document.querySelector(".todo-list#name").value.trim();
-}
-
-function addCurrentTodoList(){
-    const currentTodoList = getCurrentTodoList();
-    const ls_todoListes_JSON = localStorage.getItem("todo-listes");
-    const todoListes = JSON.parse(ls_todoListes_JSON);
-    todoListes.push(currentTodoList);
-    localStorage.setItem("todo-listes",JSON.stringify(todoListes));
+function getCurrentListId(){
+    return document.querySelector("#name-todo-list").getAttribute("data-listid");
 }
