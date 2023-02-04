872244fb1a8aa7ec9ea71c3b911446164cdc76b7
CHAOUCHI <massi.chaouchi@gmail.com>
2023-02-04 21:19:27 +0100

    Removing useless files

---
+++ monUltimeTodoList/v1/struc.js
@@ -0,0 +1,24 @@
+// Tableau de todoliste
+let Listes = [
+    //Todoliste 1
+    { 
+        name:"Titre de la to liste",
+        tasks : [
+            { text:"1kg Pommes", cross: true || false }
+        ]
+    },
+    //Todoliste 2 
+    { 
+        name:"Titre de la to liste",
+        tasks : [
+            { text:"1kg Pommes", cross: true || false }
+        ]
+    },
+    //Todoliste 3
+    { 
+        name:"Titre de la to liste",
+        tasks : [
+            { text:"1kg Pommes", cross: true || false }
+        ]
+    }
+]
